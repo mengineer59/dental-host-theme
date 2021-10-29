fc26721c932846e4028f76ff23189f3acb1e09a1
mengineer59 <muratkarakoc19971@gmail.com>
2021-10-29 12:09:41 +0300

    performance turning

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  toggleCollapse:string = "block";
+
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
 
+  onToggleCollapse(){
+    this.toggleCollapse = (this.toggleCollapse == "block" ? "none" : "block");
+  }
 }
